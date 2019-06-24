509c948a52f1bfeaaf35b69b070f158cfbdb882d
amaralc <amaral.calil@gmail.com>
2019-06-24 14:41:39 -0300

    Setup: eA is live

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.end('<h1>Hello World</h1>');
+	res.end('<h1>20190624 1440 - estado da Arte entra no ar</h1>');
 });
 
 server.listen(port,() => {
