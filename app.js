16bfea1827c2f00ca57cc32b9595cc1e551c8634
amaralc <amaral.calil@gmail.com>
2019-06-24 14:54:21 -0300

    Comunicacao: hello world e agradecimentos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.end('<h1>20190624 1440 - estado da Arte entra no ar</h1>');
+	res.end('<h1>eA </h1> <p><br>24/06/2019 14:46 - eA: estamos nas nuvens!<\p><p>24/06/2019 14:51 - eA: agradecimentos especiais a Leandro e Elias pelas dicas e palavras-chave.<\p>');	
 });
 
 server.listen(port,() => {
