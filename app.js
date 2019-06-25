328334bb4c236fa381d1b821a03455740e27ace9
amaralc <amaral.calil@gmail.com>
2019-06-25 16:33:00 -0300

    Setup: uploading p5js again

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-var path = require('path');
+// Message in the console to show that file responded
+console.log('server is starting');
 
+// Require modules
+var path = require('path');
 var express = require('express');
 
+// Declare app and point to index.ejs file
 var app = express();
-
 app.set('view engine', 'ejs');
 app.set('views',path.resolve(__dirname, 'views'));
-
 app.get('/', function(req,res){
 	res.render('index');
 });
 
+// Congig express with midleware
+app.use(express.static(path.resolve(__dirname,'public')));
+
 //app.post();
 
+//app.use(express.static(path.resolve(__dirname, 'public')))
 
-app.listen(process.env.PORT || 3000);
+// Listen to defined port and show message called by function listening
+app.listen(process.env.PORT || 3000, listening);
+
+
+// Functions
+function listening(){
+    console.log("listening...");
+}
 
 /*
 
---
+++ public/sketch.js
@@ -0,0 +1,8 @@
+function setup(){
+    createCanvas(innerWidth, 500);
+};
+
+function draw(){
+    background(50);
+    ellipse(mouseX, mouseY, 20,20);
+};
